6a58c625a796d492c1c5d6d76f2b87c4eecb49e9
ちとせあめ <chitose.dev@outlook.jp>
2025-02-06 23:52:17 +0900

    updata

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 (function(){
+    const colors={};
+    const styles={
+        bg:{
+            get [expr](){
+                console.log(expr);
+            }
+        }
+    };
     let style=`*{margin:0;padding:0;box-sizing:border-box;}
 body{font-family:Arial,sans-serif;line-height:1.5;}
 .flex{display:flex;}`;
     const classList=new Set();
     document.querySelectorAll("*").forEach(el=>el.classList.forEach(cls=>classList.add(cls)));
     style=Array.from(classList).map(cls=>{
-    }).join("");
+        const parts=cls.split("-");
+        const prefix=parts[0];
+        const value=parts[1]
+        prefix[value];
+        
+    }).filter(Boolean).join("");
     const styleSheet=document.createElement("style");
     styleSheet.innerHTML=style;
     document.head.appendChild(styleSheet);
